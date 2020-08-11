7b34f23793a098df53e3dfe03e45bc54bd0156c3
Alperen <alperen.ergel@yandex.com>
2020-08-11 21:37:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,7 @@ print("\n" + W + "\033[1;35;1mRequired Applications Downloading\n" + ENDC)
 os.system("apt-get -y install proxychains")
 os.system("apt-get -y install tor")
 os.system("apt-get -y install nmap")
+os.system("rm -rf /etc/proxychains.conf")
+os.system("mv proxy.conf /etc/proxychains.conf")
 
 print("\n" + S + "Downloading Done...\n")
